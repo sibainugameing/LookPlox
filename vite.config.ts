bb16f3cf5ae020c3633d4bfaf9596f83c4cfe4b4
ymdog <ymkzdoginu11@gmail.com>
2026-09-22 22:33:31 +0900

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+export default defineConfig({
+  plugins: [svelte()],
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: "127.0.0.1",
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+});
