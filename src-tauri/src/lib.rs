7e124247b89c752bd88fbeb66623ecbe9a1ea384
ymdog <ymkzdoginu11@gmail.com>
2026-09-23 01:07:20 +0900

    Fix mutable writer in index clearing

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -130,7 +130,7 @@ impl SearchEngine {
   }
 
   pub fn clear(&self) -> tantivy::Result<()> {
-    let writer = self
+    let mut writer = self
       .writer
       .lock()
       .map_err(|_| tantivy::TantivyError::SystemError("writer lock poisoned".into()))?;
